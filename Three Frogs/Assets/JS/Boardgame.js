09d523d987601eca12ccce0b49ab17708996a853
FullyMed <maxfelix05@gmail.com>
2025-04-16 23:19:04 +0800

    New

--- Three Frogs/Assets/JS/Boardgame.js
+++ Three Frogs/Assets/JS/Boardgame.js
@@ -190,15 +190,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-
-
-// ===============================
-// JS for Collection.html
-// ===============================
-if (window.location.pathname.includes("Collection.html")) {
-  const isLoggedIn = localStorage.getItem("loggedInUser");
-  if (!isLoggedIn) {
-    alert("You must be logged in to view the full boardgame collection.");
-    window.location.href = "Login.html";
-  }
-}
